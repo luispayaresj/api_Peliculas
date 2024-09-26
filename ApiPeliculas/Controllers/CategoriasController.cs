d68afb12ae36bc6774b56b60a2b431b9b76a602f
luispayaresj <luispj98@outlook.com>
2024-09-26 17:43:23 -0500

    Controller

---
+++ ApiPeliculas/Controllers/CategoriasController.cs
@@ -0,0 +1,40 @@
+using ApiPeliculas.Modelos.Dtos;
+using ApiPeliculas.Repositorio.IRepositorio;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPeliculas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly ICategoriaRepositorio _ctRepository;
+        private readonly IMapper _mapper;
+
+        public CategoriasController(ICategoriaRepositorio ctRepository, IMapper mapper)
+        {
+            _ctRepository = ctRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetCategorias()
+        {
+            var listaCategorias = _ctRepository.GetCategorias();
+            
+            var listaCategoriasDto = new List<CategoriaDto>();
+
+            foreach (var categoria in listaCategorias)
+            {
+                listaCategoriasDto.Add(_mapper.Map<CategoriaDto>(categoria));
+            }
+            
+            return Ok(listaCategoriasDto);
+        }
+        
+    }
+}
